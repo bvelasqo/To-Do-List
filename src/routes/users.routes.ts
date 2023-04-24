95ccf94023a8615f7854fbe51ff034dc87f22c3e
jjmo15 <josemolina1598@gmail.com>
2023-04-24 15:54:49 -0500

    encriptar password user y compararlas

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -32,6 +32,7 @@ userRouter.post("/", async (ctx) => {
   ctx.body = response;
 });
 
+
 userRouter.put("/:id", async (ctx) => {
   const userController = new UserController();
   const newUser = ctx.body.user
@@ -49,4 +50,56 @@ userRouter.delete("/:id", async (ctx) => {
   ctx.body = user;
 });
 
+// Express y bcrypts 
+const express = require('express');
+const app = express();
+const bcriptjs=require('bcryptjs');
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.post('/login', async(req, res)=>{
+    const user = req.body.user;
+    const pass = req.body.pass;
+    
+    if(user == 'admin' && pass == '12345'){
+        let passhash = await bcriptjs.hash(pass, 8);
+        res.json({
+            message:'¡Autentication exitosa!',
+            passhash:passhash
+        });
+    } else{
+        res.json({
+            message:'Ingrese correctamente sus credenciales'
+        })
+    }
+});
+
+app.post('/login', async (ctx) => {
+    const user = ctx.request.body.user;
+    const pass = ctx.request.body.pass;
+    if (user == 'admin' && pass == '12345') {
+        let passhash = await bcriptjs.hash(pass, 8);
+        ctx.body = {
+            message: '¡Autenticación exitosa!',
+            passhash: passhash
+            
+        };
+        
+    } else {
+        ctx.body = {
+            message: 'Ingrese correctamente sus credenciales'
+        };
+        
+    }
+
+});
+app.get('/compare', (req, res)=>{
+    let hashSaved = '$2a$08$Plcf52YL1JzpKAci/THxFuaRyE6ilYtnHPpgY3qqnTxdMnuOnn4y.';
+    let compare = bcriptjs.compareSync('123456', hashSaved);
+    if(compare){
+        res.json('OK');
+    }else{
+        res.json('Son diferentes ');
+    }
+});
 export default userRouter;
