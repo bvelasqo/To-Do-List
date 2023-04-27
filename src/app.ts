c5252dd1a8d192b3195be25fc8de7349edbc48b7
Juandy <juandiego0329@gmail.com>
2023-04-27 11:50:28 -0500

    Propuesta de conexion con CosmosDB

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,28 @@
-import koa from "koa";
-import bodyparser from "koa-bodyparser";
-import loggerKoa from "koa-logger";
-import cors from "koa2-cors";
-import mount from "koa-mount";
-import auth from "koa-basic-auth";
-import health from "./routes/health.routes";
-import taskRouter from "./routes/tasks.routes";
-import 'database/dynamodb' // para que dynamo corra XD
-import client from "./shared/db/postgres";
-import Router from "koa-router";
-import userRouter from "./routes/users.routes";
-import categoryRouter from "./routes/category.routes";
+import koa from 'koa';
+import bodyparser from 'koa-bodyparser';
+import loggerKoa from 'koa-logger';
+import cors from 'koa2-cors';
+import mount from 'koa-mount';
+import auth from 'koa-basic-auth';
+import health from './routes/health.routes';
+import taskRouter from './routes/tasks.routes';
+import 'database/dynamodb'; // para que dynamo corra XD
+import client from './shared/db/postgres';
+import Router from 'koa-router';
+import userRouter from './routes/users.routes';
+import categoryRouter from './routes/category.routes';
+import labelRouter from './routes/label.routes';
+import cosmosDBConnection from 'database/cosmos';
 
 // establish database connection and initialize data source FOR POSTGRES
 client
-    .initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!")
-    })
-    .catch((err) => {
-        console.error("Error during Data Source initialization:", err)
-    })
+	.initialize()
+	.then(() => {
+		console.log('Data Source has been initialized!');
+	})
+	.catch((err) => {
+		console.error('Error during Data Source initialization:', err);
+	});
 
 //init
 const app = new koa();
@@ -28,19 +30,25 @@ const app = new koa();
 app.use(cors());
 app.use(loggerKoa());
 app.use(bodyparser());
- app.use(mount("/health", auth({
-    name: 'user',
-    pass: 'password',
-})));
+app.use(
+	mount(
+		'/health',
+		auth({
+			name: 'user',
+			pass: 'password',
+		})
+	)
+);
 
 // Routes
 
 // app.use(health.routes());
 // router.use(mount('/login', login.routes()))
 // Hay que modularizar las rutas en index
-app.use(mount('/api/v1/task', taskRouter.routes()))
-app.use(mount('/api/v1/user', userRouter.routes()))
-app.use(mount('/api/v1/category', categoryRouter.routes()))
+app.use(mount('/api/v1/task', taskRouter.routes()));
+app.use(mount('/api/v1/user', userRouter.routes()));
+app.use(mount('/api/v1/category', categoryRouter.routes()));
+app.use(mount('/api/v1/label', labelRouter.routes()));
 // router.use(mount('/label', labelRouter.routes()))
 
 //export server
--- src/shared/db/cosmos.ts
+++ src/shared/db/cosmos.ts
@@ -1,2 +1,21 @@
-// CosmosDB Connection. If is possible create a clase for this to connect to the database and display a client to manage the database.
-// No create class if exists any orm for cosmos. Just connect the database.
+import mongoose from 'mongoose';
+
+const connectionString =
+	'mongodb://c1091aba-0ee0-4-231-b9ee:kEx9JWuzV8UpWIBNmj9dYTjXztl5Qci6YqiN5s9BT4fLlajEgpstvhNefW5s639vbco3SLANFl9NACDbzRleFw==@c1091aba-0ee0-4-231-b9ee.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@c1091aba-0ee0-4-231-b9ee@';
+
+mongoose.connect(connectionString, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const cosmosDBConnection = mongoose.connection;
+
+cosmosDBConnection.on('error', (error) => {
+	console.error('Error connecting to CosmosDB:', error);
+});
+
+cosmosDBConnection.once('open', () => {
+	console.log('Connected to CosmosDB');
+});
+
+export default cosmosDBConnection;
